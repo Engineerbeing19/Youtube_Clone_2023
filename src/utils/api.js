c3a960b7be4389ccfd1f2fac99178f0fd0dfc11e
harsh <vanshmishra5995@gmail.com>
2023-09-04 16:40:31 +0530

    First Version

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":"afbbd9716emshd0dda5040799394p186c67jsnd93383d6d394",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
